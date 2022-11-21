e6170f9affcdc6c4d7c75754fa8d50bd1c4d58c2
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2022-11-21 11:54:31 -0600

    added internal and external js

---
+++ js/external_js.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hello from external JavaScript');
+alert('Welcome to my Website!');
+var userInput = prompt('What is your favorite color?');
+console.log('users favorite color response was ' + userInput);
+alert('Great my favorite color is also ' + userInput);
+userInput = prompt("Did you mean blue or red?");
+alert("fine " + userInput + " will work");
